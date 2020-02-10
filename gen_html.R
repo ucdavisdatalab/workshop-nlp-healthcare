c57412ceccad80bc13a3d76ffc832a204344db8b
Arthur Koehl <avkoehl@ucdavis.edu>
2020-02-09 21:44:56 -0800

    combined all in index.html

--- gen_html.R
+++ gen_html.R
@@ -1,1 +1,3 @@
-rmarkdown::render("./dtm_tfidf_pca.Rmd", output_dir="./docs/", output_file="index.html")
+rmarkdown::render("./intro_nlp_health.Rmd", output_dir="./docs/", output_file="intro.html")
+rmarkdown::render("./dtm_tfidf_pca.Rmd", output_dir="./docs/", output_file="code.html")
+rmarkdown::render("./combined.Rmd", output_dir="./docs/", output_file="index.html")
