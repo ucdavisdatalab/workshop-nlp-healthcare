24789fe5c262e408d0c8b3ae0cd4990a9f2343d8
Wesley Brooks <wbrooks@Administrators-MacBook-Pro.local>
2020-02-09 21:58:10 -0800

    correct the name of newcorpus variable

--- docs/get_corpus.R
+++ docs/get_corpus.R
@@ -6,7 +6,7 @@ get_corpus <- function(data, remove=c("")) {
   newcorpus = tm_map(newcorpus, removeWords, stopwords("en")) # may need snowballC package
   newcorpus = tm_map(newcorpus, removeWords, remove)
   
-  dtm = DocumentTermMatrix(combined_corpus)
+  dtm = DocumentTermMatrix(newcorpus)
   tfidf = weightTfIdf(dtm, normalize=TRUE)
   articles <- t(tfidf)
   
